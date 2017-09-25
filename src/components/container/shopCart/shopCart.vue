9e556aacffccb822a448c07ca18999c5c8357daa
ClausClaus <weixinla@foxmail.com>
2017-09-25 16:30:59 +0800

    使用中间件Vue实例实现兄弟组件通信，编写小球动画，编写底部弹出层。

--- src/components/container/shopCart/shopCart.vue
+++ src/components/container/shopCart/shopCart.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="shopcart-container">
-    <div class="content">
+    <div class="content" @click="toggleList">
       <div class="content-left">
         <div class="logo-wrapper">
           <div class="logo" :class="{'highlight':totalCount > 0}">
@@ -11,16 +11,134 @@
         <div class="price" :class="{'highlight':totalPrice > 0}">￥{{totalPrice}}</div>
         <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
       </div>
-      <div class="content-right">
+      <div class="content-right" @click.stop.prevent="pay">
         <div class="pay" :class="payClass">{{payDesc}}</div>
       </div>
     </div>
+    <div class="ball-container">
+      <transition-group name="drop" @before-enter="ballBeforeEnter" @enter="ballEnter" @after-enter="ballAfterEnter" :css="false">
+        <template v-for="(ball,index) in balls">
+          <div class="ball" v-show="ball.show" :key="index">
+            <div class="inner inner-hook"></div>
+          </div>
+        </template>
+      </transition-group>
+    </div>
+    <transition name="fold">
+      <div class="shopcart-list" v-show="listshow">
+        <div class="list-header">
+          <h1 class="title">购物车</h1>
+          <span class="empty" @click="empty">清空</span>
+        </div>
+        <div class="list-content" ref="listContent">
+          <ul>
+            <template v-for="(food,index) in selectFoods">
+              <li class="food-item" :key="index">
+                <span class="name">{{food.name}}</span>
+                <div class="cart-control-wrapper">
+                  <cartControl :food="food"></cartControl>
+                </div>
+                <div class="price">
+                  <span class="price-result">￥{{food.price * food.count}}</span>
+                </div>
+              </li>
+            </template>
+          </ul>
+        </div>
+      </div>
+    </transition>
+    <transition name="fade">
+      <div class="list-mask" @click="hideList" v-show="listshow"></div>
+    </transition>
   </div>
 </template>
 <script type="text/ecmascript-6">
+import BScroll from 'better-scroll';
+import Connect from '../../Connect'; // 兄弟组件之间的通信，中间件Vue实例
+import cartControl from '../../container/cartControl/cartControl.vue';
 export default {
   data() {
     return {
+      balls: [{ show: false }, { show: false }, { show: false }, { show: false }, { show: false }, { show: false }, { show: false }],
+      dropBalls: [], // 已经下落的小球
+      fold: true,
+    }
+  },
+  created() {
+    Connect.$on('addCart', (target) => {
+      this.drop(target);
+    })
+  },
+  methods: {
+    toggleList() {
+      if (!this.totalCount) return;
+      this.fold = !this.fold;
+    },
+    drop(el) {
+      // 体验优化，异步加载
+      this.$nextTick(() => {
+        for (var i = 0; i < this.balls.length; i++) {
+          var ball = this.balls[i];
+          if (!ball.show) {
+            ball.show = true;
+            ball.el = el;
+            this.dropBalls.push(ball);
+            return;
+          }
+        }
+      })
+    },
+    // 动画函数三个钩子
+    ballBeforeEnter(el) {
+      let count = this.balls.length;
+      while (count--) {
+        let ball = this.balls[count];
+        if (ball.show) {
+          let rect = ball.el.getBoundingClientRect();
+          let x = rect.left - 32;
+          let y = -(window.innerHeight - rect.top - 22);
+          el.style.display = ""; // 让元素先显示出来
+          el.style.webkitTransform = `translate3d(0,${y}px,0)`;
+          el.style.transform = `translate3d(0,${y}px,0)`;
+          let inner = el.getElementsByClassName('inner-hook')[0];
+          inner.style.webkitTransform = `translate3d(${x}px,0,0)`;
+          inner.style.transform = `translate3d(${x}px,0,0)`;
+        }
+      }
+    },
+    ballEnter(el, done) {
+      // 触发浏览器重绘
+      let elHeight = el.offsetHeight;
+      // 浏览器重绘之后
+      this.$nextTick(() => {
+        el.style.webkitTransform = 'translate3d(0,0,0)';
+        el.style.transform = 'translate3d(0,0,0)';
+        let inner = el.getElementsByClassName('inner-hook')[0];
+        inner.style.webkitTransform = 'translate3d(0,0,0)';
+        inner.style.transform = 'translate3d(0,0,0)';
+      });
+      done();
+    },
+    ballAfterEnter(el) {
+      let ball = this.dropBalls.shift();
+      if (ball) {
+        setTimeout(function() {
+          ball.show = false;
+          el.style.display = 'none';
+        }, 400)
+      }
+    },
+    empty() {
+      this.selectFoods.forEach(function(food) {
+        food.count = 0;
+      });
+    },
+    hideList() {
+      this.fold = true;
+    },
+    pay() {
+      if (this.totalPrice < this.minPrice) return;
+      window.alert(`需要支付${this.totalPrice}元`);
     }
   },
   computed: {
@@ -54,6 +172,26 @@ export default {
       } else {
         return 'enough';
       }
+    },
+    listshow() {
+      if (!this.totalCount) {
+        this.fold = true;
+        return false;
+      }
+      let show = !this.fold;
+      if (show) {
+        this.$nextTick(() => {
+          if (!this.listContent) {
+            this.listContent = new BScroll(this.$refs.listContent, {
+              click: true,
+              bounce: true
+            });
+          } else {
+            this.listContent.refresh();
+          }
+        })
+      }
+      return show;
     }
   },
   props: {
@@ -61,10 +199,11 @@ export default {
       type: Array,
       default() {
         return [
-          {
-            price: 20,
-            count: 1
-          }
+          //     数据格式
+          // {
+          //   price: 20,
+          //   count: 1
+          // }
         ]
       }
     },
@@ -76,13 +215,17 @@ export default {
       type: Number,
       default: 0
     }
+  },
+  components: {
+    cartControl
   }
 }
 </script>
 <style lang="less" type="text/less" rel="stylesheet/less">
+@import url("../../../../src/common/less/mixin/mixin.less");
 .shopcart-container {
   position: fixed;
-  bottom:0;
+  bottom: 0;
   left: 0;
   right: 0;
   z-index: 50;
@@ -183,6 +326,105 @@ export default {
           color: #ffffff;
         }
       }
+    }
+  }
+  .ball-container {
+    .ball {
+      position: fixed;
+      left: 32px;
+      bottom: 22px;
+      z-index: 200;
+      transition: all .4s cubic-bezier(.58, -0.41, .85, .24);
+      .inner {
+        width: 16px;
+        height: 16px;
+        border-radius: 50%;
+        background-color: rgb(0, 160, 220);
+        transition: all .4s linear;
+      }
+    }
+  }
+  .shopcart-list {
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: -1;
+    width: 100%;
+    transition: all .4s linear;
+    transform: translate3d(0, -100%, 0);
+    &.fold-enter-active,
+    &.fold-leave-active {
+      transform: translate3d(0, 0, 0);
+    }
+    .list-header {
+      height: 40px;
+      line-height: 40px;
+      padding: 0 18px;
+      background-color: #f3f5f7;
+      border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+      .title {
+        float: left;
+        font-size: 14px;
+        color: rgb(7, 17, 27);
+      }
+      .empty {
+        float: right;
+        font-size: 12px;
+        color: rgb(0, 160, 220)
+      }
+    }
+    .list-content {
+      padding: 0 18px;
+      max-height: 217px;
+      overflow: hidden;
+      background-color: #fff;
+      .food-item {
+        position: relative;
+        padding: 12px 0;
+        box-sizing: border-box;
+        .border-1px(rgba(7, 17, 27, .1));
+        .name {
+          line-height: 24px;
+          font-size: 14px;
+          color: rgb(7, 17, 27);
+        }
+        .price {
+          position: absolute;
+          right: 90px;
+          bottom: 12px;
+          line-height: 24px;
+          font-size: 14px;
+          color: rgb(240, 20, 20);
+          .price-result {
+            font-weight: bold;
+          }
+        }
+        .cart-control-wrapper {
+          position: absolute;
+          right: 0;
+          bottom: 6px;
+        }
+      }
+    }
+  }
+  .list-mask {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -2;
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+    background-color: rgba(7, 17, 27, .6);
+    &.fade-enter-active,
+    &.fade-leave-active {
+      transition: opacity .4s linear .7s;
+    }
+    &.fade-enter,
+    &.fade-leave-to {
+      transition: opacity .4s linear;
+      opacity: 0;
     }
   }
 }
